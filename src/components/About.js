087955931ee20dcd7d628e5ac55eaa080d04fb2b
Dominic Kipkorir <harsh979pal@gamil.com>
2025-04-10 19:37:33 +0300

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { image } from "../data/user"; // Import the image from user.js
 
 function About() {
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <h1>About Me</h1>
+      <img src={image} alt="Profile" />
     </div>
   );
 }
+
+export default About;
